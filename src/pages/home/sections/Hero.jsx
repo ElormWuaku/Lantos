ac4f33ab805506ae0028fb1ed31b4730e44dffc2
doreen wuaku <hageelorm@gmail.com>
2025-02-25 11:46:55 +0000

    Project changed from ReadyRent to LCC

--- src/pages/home/sections/Hero.jsx
+++ src/pages/home/sections/Hero.jsx
@@ -1,8 +1,8 @@
-
-
 const Hero = () => {
   return (
-  Hero
+    <div className="p-6 flex justify-center items-center align-middle h-[60vh] border-b-2">
+      <h4 className="text-4xl font-bold">Hero</h4>
+    </div>
   );
 };
 
