1083ba39287d4aaa87c11948db145740286757fd
doreen wuaku <hageelorm@gmail.com>
2025-02-28 00:55:19 +0000

    New images added to about section

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,6 +2,7 @@ import hero from './hero.jpg';
 import logo from './logo.png';
 import hero2 from './hero2.jpg';
 import table from './table.png';
+import table2 from './table2.jpg';
 import yellow from './yellow.jpg';
 import craftsman from './craftsman.jpg';
 import about from './about.jpg';
@@ -11,4 +12,5 @@ import wood from './wood.jpg';
 import product from './product.jpg';
 import art from './art.jpg';
 
-export { hero, logo, hero2, table, yellow, craftsman, about, logo2, hero4, wood, product, art };
+
+export { hero, logo, hero2, table, table2, yellow, craftsman, about, logo2, hero4, wood, product, art };
